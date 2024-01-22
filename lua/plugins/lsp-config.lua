780c91dcb5054efbad63984c96fb351519e5ceb4
Jodocus97 <me@marxpatrick.de>
2024-01-23 00:22:44 +0100

    added some plugins and configurations

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,17 +10,24 @@ return{
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed =  {"lua_ls"}
+        ensure_installed =  {"lua_ls", "texlab"}
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false, 
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.texlab.setup({
+        capabilities = capabilities
+      })
+      vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
